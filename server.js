aec8ce8a8546978525218b737843bc4ce82a6f73
Lantao home PC <lantaook@gmail.com>
2023-01-28 22:02:49 -0500

    figure out have to put use static at the bottom and set up some easy url

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const PORT = 3002;
+const app = express();
+
+
+app.get("/", (req, res) => {
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname,'db/db.json'))
+})
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log("Server is listening...");
+});
