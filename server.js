6607662e663fc259931f497c8fdd599e3896358d
Lantao home PC <lantaook@gmail.com>
2023-02-01 22:57:51 -0500

    save heroku port var

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const express = require("express");
 const path = require("path");
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.json());
